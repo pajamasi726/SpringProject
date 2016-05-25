e34561fc9abaf9378119f3ff40aff65a2cdada71
ysk <ysk@sk.com>
2016-05-25 15:58:37 +0900

    전체 리스트와 읽기 페이지 작성 완료

---
+++ MVC_Board/src/main/java/com/mvc/board/BoardController.java
@@ -0,0 +1,57 @@
+package com.mvc.board;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.mvc.board.DTO.BoardDTO;
+import com.mvc.board.service.BoardService;
+
+
+@Controller
+@RequestMapping("/board/*")
+public class BoardController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(BoardController.class);
+	
+	@Autowired
+	private BoardService service;
+	
+	@RequestMapping(value = "/register", method = RequestMethod.GET)
+	public void registerGET()
+	{
+		logger.info("register get.....");
+		
+	}
+	
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String registerPOST(BoardDTO dto , RedirectAttributes rttr) throws Exception
+	{
+		logger.info("register post......");
+		logger.info(dto.toString());
+		service.create(dto); 
+		rttr.addFlashAttribute("result", "success");
+		
+		return "redirect:/board/listAll";
+	}
+	
+	@RequestMapping(value = "/listAll", method = RequestMethod.GET)
+	public void listAll(Model model)throws Exception
+	{
+		logger.info("show all list... ... ...");
+		model.addAttribute("list", service.listAll() );
+	}
+	
+	@RequestMapping(value = "/read", method = RequestMethod.GET)
+	public void read(@RequestParam("bno") int bno, Model model) throws Exception
+	{
+		model.addAttribute(service.read(bno));
+	}
+	
+}
